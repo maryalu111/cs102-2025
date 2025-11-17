aa493e3b2c80589c9c0d515e7bc70cff510523b5
maryalu111 <maryalu@inbox.ru>
2025-11-17 19:48:33 +0300

    Реализованы функции encrypt_vigenere() и decrypt_vigenere()

--- homework01/vigenere.py
+++ homework01/vigenere.py
@@ -10,6 +10,22 @@ def encrypt_vigenere(plaintext: str, keyword: str) -> str:
     """
     ciphertext = ""
     # PUT YOUR CODE HERE
+    alphabet = 26  # 26 - количество букв в английском алфавите
+    A_num = ord("A")
+    a_num = ord("a")
+    keyword = keyword.upper()
+    for i, char in enumerate(plaintext):
+        if char.isalpha():
+            key_char = keyword[i % len(keyword)]
+            shift = ord(key_char) - A_num
+            if char.isupper():
+                size = A_num
+            else:
+                size = a_num
+            position = (ord(char) - size + shift) % alphabet
+            ciphertext += chr(size + position)
+        else:
+            ciphertext += char
     return ciphertext
 
 
@@ -25,4 +41,20 @@ def decrypt_vigenere(ciphertext: str, keyword: str) -> str:
     """
     plaintext = ""
     # PUT YOUR CODE HERE
+    alphabet = 26  # 26 - количество букв в английском алфавите
+    A_num = ord("A")
+    a_num = ord("a")
+    keyword = keyword.upper()
+    for i, char in enumerate(ciphertext):
+        if char.isalpha():
+            key_char = keyword[i % len(keyword)]
+            shift = ord(key_char) - A_num
+            if char.isupper():
+                size = A_num
+            else:
+                size = a_num
+            position = (ord(char) - size - shift) % alphabet
+            plaintext += chr(size + position)
+        else:
+            plaintext += char
     return plaintext
