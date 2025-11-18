8b50c2975da7996319c9321053aed9ef257fb024
maryalu111 <maryalu@inbox.ru>
2025-11-18 12:50:16 +0300

    Реализована функция decrypt_poly_shift()

--- homework01/caesar.new.py
+++ homework01/caesar.new.py
@@ -1,10 +1,10 @@
 def decrypt_poly_shift(ciphertext: str, odd_shift: int, even_shift: int) -> str:
     plaintext = ""
     alphabet = 32  # Количество букв в русском алфавите (без Ё/ё)
-    text_to_decrypt = plaintext.replace("Ё", "Е").replace("ё", "е")
+    text_to_decrypt = ciphertext.replace("Ё", "Е").replace("ё", "е")
     upper_a_num = ord("А")
     lower_a_num = ord("а")
-    for index, elem in enumerate(ciphertext):
+    for index, elem in enumerate(text_to_decrypt):
         if "а" <= elem <= "я" or "А" <= elem <= "Я":
             register = lower_a_num if elem.islower() else upper_a_num
             shift = odd_shift if index % 2 == 1 else even_shift
