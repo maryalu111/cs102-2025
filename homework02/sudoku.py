f74326949f722bcd051dea5fdaabbd89ae8e75b4
maryalu111 <maryalu@inbox.ru>
2025-12-01 00:58:50 +0300

    Реализованы все функции

--- homework02/sudoku.py
+++ homework02/sudoku.py
@@ -1,11 +1,12 @@
 import pathlib
+import random
 import typing as tp
 
 T = tp.TypeVar("T")
 
 
 def read_sudoku(path: tp.Union[str, pathlib.Path]) -> tp.List[tp.List[str]]:
-    """ Прочитать Судоку из указанного файла """
+    """Прочитать Судоку из указанного файла"""
     path = pathlib.Path(path)
     with path.open() as f:
         puzzle = f.read()
@@ -19,15 +20,11 @@ def create_grid(puzzle: str) -> tp.List[tp.List[str]]:
 
 
 def display(grid: tp.List[tp.List[str]]) -> None:
-    """Вывод Судоку """
+    """Вывод Судоку"""
     width = 2
     line = "+".join(["-" * (width * 3)] * 3)
     for row in range(9):
-        print(
-            "".join(
-                grid[row][col].center(width) + ("|" if str(col) in "25" else "") for col in range(9)
-            )
-        )
+        print("".join(grid[row][col].center(width) + ("|" if str(col) in "25" else "") for col in range(9)))
         if str(row) in "25":
             print(line)
     print()
@@ -106,7 +103,7 @@ def find_empty_positions(grid: tp.List[tp.List[str]]) -> tp.Optional[tp.Tuple[in
     for i in range(len(grid)):
         for j in range(len(grid[i])):
             if grid[i][j] == ".":
-                return(i, j)
+                return (i, j)
     return None
 
 
@@ -130,7 +127,7 @@ def find_possible_values(grid: tp.List[tp.List[str]], pos: tp.Tuple[int, int]) -
 
 
 def solve(grid: tp.List[tp.List[str]]) -> tp.Optional[tp.List[tp.List[str]]]:
-    """ Решение пазла, заданного в grid """
+    """Решение пазла, заданного в grid"""
     """ Как решать Судоку?
         1. Найти свободную позицию
         2. Найти все возможные значения, которые могут находиться на этой позиции
@@ -158,9 +155,53 @@ def solve(grid: tp.List[tp.List[str]]) -> tp.Optional[tp.List[tp.List[str]]]:
 
 
 def check_solution(solution: tp.List[tp.List[str]]) -> bool:
-    """ Если решение solution верно, то вернуть True, в противном случае False """
+    """Если решение solution верно, то вернуть True, в противном случае False"""
     # TODO: Add doctests with bad puzzles
     pass
+    """ Если решение solution верно, то вернуть True, в противном случае False 
+    >>> right_solution = [
+        ["5", "3", "4", "6", "7", "8", "9", "1", "2"],
+    ... ["6", "7", "2", "1", "9", "5", "3", "4", "8"],
+    ... ["1", "9", "8", "3", "4", "2", "5", "6", "7"],
+    ... ["8", "5", "9", "7", "6", "1", "4", "2", "3"],
+    ... ["4", "2", "6", "8", "5", "3", "7", "9", "1"],
+    ... ["7", "1", "3", "9", "2", "4", "8", "5", "6"],
+    ... ["9", "6", "1", "5", "3", "7", "2", "8", "4"],
+    ... ["2", "8", "7", "4", "1", "9", "6", "3", "5"],
+    ... ["3", "4", "5", "2", "8", "6", "1", "7", "9"],
+        ]
+    >>> check_solution(right_solution)
+    True
+
+    >>> bad_solution = [
+        ["6", "6", "1", "1", "1", "5", "8", "3", "7"],
+    ... ["3", "5", "7", "8", "2", "6", "1", "4", "9"],
+    ... ["1", "4", "8", "9", "3", "7", "5", "2", "6"],
+    ... ["6", "3", "9", "5", "1", "2", "4", "7", "8"],
+    ... ["5", "8", "1", "7", "6", "4", "3", "9", "2"],
+    ... ["4", "7", "2", "3", "9", "8", "6", "1", "5"],
+    ... ["9", "6", "4", "2", "8", "3", "7", "5", "1"],
+    ... ["8", "1", "5", "4", "7", "9", "2", "6", "3"],
+    ... ["7", "2", "3", "6", "5", "1", "9", "8", "4"],
+    ]
+    >>> check_solution(bad_solution)
+    False
+    """
+
+    correct_values = set(str(i) for i in range(1, 10))
+    for i in range(len(solution)):
+        row = set(get_row(solution, (i, 0)))
+        col = set(get_col(solution, (0, i)))
+        if row != correct_values or col != correct_values:
+            return False
+
+    for i in range(0, len(solution), 3):
+        for j in range(0, len(solution), 3):
+            block = set(get_block(solution, (i, j)))
+            if block != correct_values:
+                return False
+
+    return True
 
 
 def generate_sudoku(N: int) -> tp.List[tp.List[str]]:
@@ -185,6 +226,14 @@ def generate_sudoku(N: int) -> tp.List[tp.List[str]]:
     True
     """
     pass
+    generated_sudoku = [["." for _ in range(9)] for _ in range(9)]
+    solve(generated_sudoku)
+    positions = [(x, y) for x in range(9) for y in range(9)]
+    random.shuffle(positions)
+    for i in range(81 - N):
+        x, y = positions[i]
+        generated_sudoku[x][y] = "."
+    return generated_sudoku
 
 
 if __name__ == "__main__":
