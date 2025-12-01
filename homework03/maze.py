5e4cd98f66d34a980c0f5f8fbd8e51b6bd71bc4b
maryalu111 <maryalu@inbox.ru>
2025-12-01 23:25:14 +0300

    Реализованы все функции

--- homework03/maze.py
+++ homework03/maze.py
@@ -9,9 +9,7 @@ def create_grid(rows: int = 15, cols: int = 15) -> List[List[Union[str, int]]]:
     return [["■"] * cols for _ in range(rows)]
 
 
-def remove_wall(
-    grid: List[List[Union[str, int]]], coord: Tuple[int, int]
-) -> List[List[Union[str, int]]]:
+def remove_wall(grid: List[List[Union[str, int]]], coord: Tuple[int, int]) -> List[List[Union[str, int]]]:
     """
 
     :param grid:
@@ -20,11 +18,23 @@ def remove_wall(
     """
 
     pass
+    x, y = coord
+    index_last_col = len(grid[0]) - 1
+    direction = choice(("up", "right"))
+    if direction == "up":
+        if x > 1:
+            grid[x - 1][y] = " "
+        elif y < index_last_col - 1:
+            grid[x][y + 1] = " "
+    else:
+        if y < index_last_col - 1:
+            grid[x][y + 1] = " "
+        elif x > 1:
+            grid[x - 1][y] = " "
+    return grid
 
 
-def bin_tree_maze(
-    rows: int = 15, cols: int = 15, random_exit: bool = True
-) -> List[List[Union[str, int]]]:
+def bin_tree_maze(rows: int = 15, cols: int = 15, random_exit: bool = True) -> List[List[Union[str, int]]]:
     """
 
     :param rows:
@@ -47,6 +57,8 @@ def bin_tree_maze(
     # выбрать второе возможное направление
     # 3. перейти в следующую клетку, сносим между клетками стену
     # 4. повторять 2-3 до тех пор, пока не будут пройдены все клетки
+    for current_cell in empty_cells:
+        remove_wall(grid, current_cell)
 
     # генерация входа и выхода
     if random_exit:
@@ -70,6 +82,15 @@ def get_exits(grid: List[List[Union[str, int]]]) -> List[Tuple[int, int]]:
     """
 
     pass
+    exits = []
+
+    for x in range(len(grid)):
+        for y in range(len(grid[x])):
+            if grid[x][y] == "X":
+                exits.append((x, y))
+                if len(exits) == 2:
+                    return exits
+    return exits
 
 
 def make_step(grid: List[List[Union[str, int]]], k: int) -> List[List[Union[str, int]]]:
@@ -81,6 +102,20 @@ def make_step(grid: List[List[Union[str, int]]], k: int) -> List[List[Union[str,
     """
 
     pass
+    number_rows = len(grid)
+    number_cols = len(grid[0])
+
+    for x in range(number_rows):
+        for y in range(number_cols):
+            if grid[x][y] == k:
+                k += 1
+                neighbors = [(x, y + 1), (x, y - 1), (x + 1, y), (x - 1, y)]
+                for coord_x, coord_y in neighbors:
+                    if 0 <= coord_x < number_rows and 0 <= coord_y < number_cols and (grid[coord_x][coord_y] == 0):
+                        grid[coord_x][coord_y] = k
+                k -= 1
+
+    return grid
 
 
 def shortest_path(
@@ -93,6 +128,23 @@ def shortest_path(
     :return:
     """
     pass
+    number_rows = len(grid)
+    number_cols = len(grid[0])
+
+    x, y = exit_coord
+    k = int(grid[x][y])
+    path = [(x, y)]
+    while grid[x][y] != 1:
+        k -= 1
+        if k < 1:
+            break
+        neighbors = [(x, y + 1), (x, y - 1), (x + 1, y), (x - 1, y)]
+        for coord_x, coord_y in neighbors:
+            if 0 <= coord_x < number_rows and 0 <= coord_y < number_cols and grid[coord_x][coord_y] == k:
+                path.append((coord_x, coord_y))
+                x, y = coord_x, coord_y
+                break
+    return path
 
 
 def encircled_exit(grid: List[List[Union[str, int]]], coord: Tuple[int, int]) -> bool:
@@ -104,6 +156,20 @@ def encircled_exit(grid: List[List[Union[str, int]]], coord: Tuple[int, int]) ->
     """
 
     pass
+    number_rows = len(grid)
+    number_cols = len(grid[0])
+    x, y = coord
+    if (x == 0 or x == number_rows - 1) and (y == 0 or y == number_cols - 1):
+        return True
+    if x == 0 and grid[x + 1][y] != " ":
+        return True
+    if x == number_rows - 1 and grid[x - 1][y] != " ":
+        return True
+    if y == 0 and grid[x][y + 1] != " ":
+        return True
+    if y == number_cols - 1 and grid[x][y - 1] != " ":
+        return True
+    return False
 
 
 def solve_maze(
@@ -116,6 +182,33 @@ def solve_maze(
     """
 
     pass
+    exits = get_exits(grid)
+
+    if len(exits) == 1:
+        return grid, exits
+
+    for poss_exit in exits:
+        if encircled_exit(grid, poss_exit):
+            return grid, None
+    for x in range(len(grid)):
+        for y in range(len(grid[0])):
+            if grid[x][y] == " ":
+                grid[x][y] = 0
+
+    start_x, start_y = exits[0]
+    finish_x, finish_y = exits[1]
+
+    grid[start_x][start_y] = 1
+    grid[finish_x][finish_y] = 0
+    k = 1
+
+    while grid[finish_x][finish_y] == 0:
+        make_step(grid, k)
+        k += 1
+
+    path = shortest_path(grid, (finish_x, finish_y))
+
+    return grid, path
 
 
 def add_path_to_grid(
--- homework03/maze_gui.py
+++ homework03/maze_gui.py
@@ -1,7 +1,9 @@
 import tkinter as tk
+from copy import deepcopy
+from tkinter import messagebox, ttk
 from typing import List
-from tkinter import ttk, messagebox
-from maze import bin_tree_maze, solve_maze, add_path_to_grid
+
+from maze import add_path_to_grid, bin_tree_maze, solve_maze
 
 
 def draw_cell(x, y, color, size: int = 10):
@@ -16,11 +18,11 @@ def draw_maze(grid: List[List[str]], size: int = 10):
     for x, row in enumerate(grid):
         for y, cell in enumerate(row):
             if cell == " ":
-                color = 'White'
+                color = "White"
             elif cell == "■":
-                color = 'black'
+                color = "black"
             elif cell == "X":
-                color = "red"
+                color = "blue"
             draw_cell(y, x, color, size)
 
 
@@ -34,14 +36,17 @@ def show_solution():
 
 
 if __name__ == "__main__":
-    global GRID, CELL_SIZE
     N, M = 51, 77
 
     CELL_SIZE = 10
     GRID = bin_tree_maze(N, M)
+    trying_grid = bin_tree_maze(N, M)
+    while not solve_maze(deepcopy(trying_grid))[1]:
+        try_grid = bin_tree_maze(N, M)
+    GRID = trying_grid
 
     window = tk.Tk()
-    window.title('Maze')
+    window.title("Maze")
     window.geometry("%dx%d" % (M * CELL_SIZE + 100, N * CELL_SIZE + 100))
 
     canvas = tk.Canvas(window, width=M * CELL_SIZE, height=N * CELL_SIZE)
@@ -51,4 +56,3 @@ if __name__ == "__main__":
     ttk.Button(window, text="Solve", command=show_solution).pack(pady=20)
 
     window.mainloop()
-
