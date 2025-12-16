4edeca65f9aba4d4229cfbfb4935879ff15c945c
maryalu111 <maryalu@inbox.ru>
2025-12-16 12:48:21 +0300

    Исправлены ошибки

--- homework04/life_gui.py
+++ homework04/life_gui.py
@@ -2,10 +2,8 @@ import random
 from pathlib import Path
 
 import pygame
-from pygame.locals import (K_SPACE, KEYDOWN, MOUSEBUTTONDOWN, QUIT, K_l, K_r,
-                           K_s)
-
 from life import GameOfLife
+from pygame.locals import K_SPACE, KEYDOWN, MOUSEBUTTONDOWN, QUIT, K_l, K_r, K_s
 from ui import UI
 
 
